2ae062f4524eba21cd2f88f8a0e62239e6dafb9d
luciadelafuente <luciadelaf1991@gmail.com>
2024-05-25 10:00:16 +0200

    visual changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -19,28 +19,28 @@ function Form({form, updateAvatar, formData, postData, url}) {
 
   return (
     <form className="addForm">
-      <h2 className="title">¡Te invitamos a nuestra boda!</h2>
+      <h2 className="title">¡CREA TU INVITACIÓN DE BODA!</h2>
       <fieldset className="addForm__group">
-        <legend className="addForm__title">Te contamos sobre el encuentro</legend>
+        <legend className="addForm__title">Información del encuentro</legend>
         <input className="addForm__input" type="text" value={formData.name} name="name" id="name" placeholder="Nombre de los invitados" onChange={handleInputChange}/>
         <input className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Fecha del enlace" value={formData.slogan} onChange={handleInputChange}/>
         <div className="addForm__2col">
-          <input className="addForm__input" type="url" name="repo" id="repo" placeholder="Comparte la url de la ubicación del evento" onChange={handleInputChange}/> {/* url de lista de regalo, google maps */}
-          <input className="addForm__input" type="url" name="demo" id="demo" placeholder="Indica la url lista de regalos" onChange={handleInputChange}/>
+          <input className="addForm__input" type="url" name="repo" id="repo" placeholder="Url de la ubicación del evento" onChange={handleInputChange}/> {/* url de lista de regalo, google maps */}
+          <input className="addForm__input" type="url" name="demo" id="demo" placeholder="Url de tu lista de regalos" onChange={handleInputChange}/>
         </div>         
-        <input className="addForm__input" type="text" value={formData.technologies} name="technologies" id="technologies" placeholder="Escribe aquí la ubicación." onChange={handleInputChange}/> {/* podría ser un select de si necesitas bus o no */}
-        <textarea className="addForm__input" type="text" name="desc" id="desc" placeholder="Dedica unas palabras a tus invitados" value={formData.desc} rows="5" onChange={handleInputChange}></textarea>
+        <input className="addForm__input" type="text" value={formData.technologies} name="technologies" id="technologies" placeholder="Escribe aquí cómo quieres que contacten tus invitados." onChange={handleInputChange}/> {/* podría ser un select de si necesitas bus o no */}
+        <textarea className="addForm__input" type="text" name="desc" id="desc" placeholder="Dedica unas palabras a tus invitados." value={formData.desc} rows="5" onChange={handleInputChange}></textarea>
       </fieldset>
 
       <fieldset className="addForm__group">
         <legend className="addForm__title">Cuéntanos sobre el enlace</legend>
-        <input className="addForm__input" type="text" value={formData.autor} name="autor" id="autor" placeholder="Vuestros nombres" onChange={handleInputChange}/>
+        <input className="addForm__input" type="text" value={formData.autor} name="autor" id="autor" placeholder="Vuestros nombres." onChange={handleInputChange}/>
         <input className="addForm__input" type="text" value={formData.job} name="job" id="job" placeholder="Una frase que os represente. Ej: ¡Hasta el infinito y más alla!" onChange={handleInputChange}/>
       </fieldset>
 
       <fieldset className="addForm__group--upload">
-        <Button  labelText="Subir foto del proyecto"   id="image"updateAvatar={updateAvatar}/>
-        <Button  labelText="Subir foto de la autora"   id="photo" updateAvatar={updateAvatar}/>
+        <Button  labelText="Subir foto que os represente"   id="image"updateAvatar={updateAvatar}/>
+        <Button  labelText="Subir foto vuestra"   id="photo" updateAvatar={updateAvatar}/>
         <button className="button--large" onClick= {handleClickUrl}>Crear invitación</button>
         <p>{url}</p>
       </fieldset>
