4d5181078b518de653d8c99804d241fe8fe4beda
Valentina <valenovalles@gmail.com>
2024-05-26 21:38:07 +0200

    link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import logoSponsor from '/images/logo-wedding.png';
 function Header() {
   return (
     <header className="header">
-          <a className="header__brand" href="./" title="Haz click para volver a la página inicial">
+          <a className="header__brand" href="/project-promo-X-module-3-team-3/" title="Haz click para volver a la página inicial">
        
-            <a className="header__title" href="./">Ada_Moon_Wedding</a>
+            <a className="header__title" href="/project-promo-X-module-3-team-3/">Ada_Moon_Wedding</a>
           </a>
           <a href="/"><img className="logoSponsor" src={logoSponsor} alt="Logo"/></a>
     </header>
